ff23de48921ee3f9e688b3a42bb483de722a7921
mateus-sartorio <mateussartorio39@gmail.com>
2024-12-21 23:41:26 +0000

    Feat: implement GET /api/stock-prices endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,28 @@
 'use strict';
 
-module.exports = function (app) {
+require('dotenv').config();
 
+module.exports = function (app) {
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function (req, res) {
+      try {
+        const { stock, like } = req.query;
+
+        const response = await fetch(`${process.env.FREECODECAMP_API_BASE_URL}/v1/stock/${stock}/quote`);
+        const data = await response.json();
+
+        const responsePayload = {
+          stockData: {
+            stock,
+            price: data.latestPrice,
+            likes: 0
+          }
+        };
+
+        res.json(responsePayload);
+      }
+      catch(e) {
+        console.log("error: ", e);
+      }
     });
-    
 };
