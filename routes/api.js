98d072e4347556fc70d340cec9e4436697123bea
mateus-sartorio <mateussartorio39@gmail.com>
2024-12-22 00:19:50 +0000

    Feat: stock likes are persisted on database

--- routes/api.js
+++ routes/api.js
@@ -2,24 +2,43 @@
 
 require('dotenv').config();
 
-console.log("what")
-
 module.exports = function (app, dbClient) {
   app.route('/api/stock-prices')
     .get(async function (req, res) {
-      console.log("got here");
+      const dbCollection = dbClient.db("information-security").collection("stocks");
 
       try {
         const { stock, like } = req.query;
 
         const response = await fetch(`${process.env.FREECODECAMP_API_BASE_URL}/v1/stock/${stock}/quote`);
+        
+        if(like === 'true') {
+          const dbStock = await dbCollection.findOne({
+            stock
+          });
+
+          if(!dbStock) {
+            await dbCollection.insertOne({
+              stock,
+              likes: 1
+            });
+          }
+          else {
+            await dbCollection.updateOne({ stock }, {
+              $inc: { likes: 1 }
+            })
+          }
+        }
+
+        const dbStock = await dbCollection.findOne({ stock });
+        
         const data = await response.json();
 
         const responsePayload = {
           stockData: {
             stock,
             price: data.latestPrice,
-            likes: 0
+            likes: dbStock?.likes ?? 0
           }
         };
 
