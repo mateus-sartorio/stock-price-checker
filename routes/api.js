168c067839b4f498a2903f91518c6b9cfddf9462
mateus-sartorio <mateussartorio39@gmail.com>
2024-12-22 18:27:01 +0000

    Feat: user list of likes is persisted on database

--- routes/api.js
+++ routes/api.js
@@ -1,38 +1,61 @@
 'use strict';
 
+const crypto = require("crypto");
+
 require('dotenv').config();
 
 module.exports = function (app, dbClient) {
   app.route('/api/stock-prices')
     .get(async function (req, res) {
-      const dbCollection = dbClient.db("information-security").collection("stocks");
+      const stocksCollection = dbClient.db("stock-price-checker").collection("stocks");
+      const usersCollection = dbClient.db("stock-price-checker").collection("users");
 
       try {
         const { stock, like } = req.query;
 
+        const ip = req.header("x-forwarded-for");
+
+        const ipHash = crypto.createHash('md5').update(ip).digest('hex');
+
         const response = await fetch(`${process.env.FREECODECAMP_API_BASE_URL}/v1/stock/${stock}/quote`);
-        
-        if(like === 'true') {
-          const dbStock = await dbCollection.findOne({
-            stock
+        const data = await response.json();
+
+        if(data === "Invalid symbol") {
+          throw new Error("Invalid symbol");
+        }
+
+        let dbUser = await usersCollection.findOne({ ipHash });
+
+        if(!dbUser) {
+          await usersCollection.insertOne({
+            ipHash,
+            likes: []
           });
 
+          dbUser = await usersCollection.findOne({ ipHash });
+        }
+        
+        if(like === 'true' && !dbUser.likes.includes(stock)) {
+          const dbStock = await stocksCollection.findOne({ stock });
+
           if(!dbStock) {
-            await dbCollection.insertOne({
+            await stocksCollection.insertOne({
               stock,
               likes: 1
             });
           }
           else {
-            await dbCollection.updateOne({ stock }, {
+            await stocksCollection.updateOne({ stock }, {
               $inc: { likes: 1 }
             })
           }
+
+          await usersCollection.updateOne({ ipHash }, {
+            $push: { likes: stock }
+          });
         }
 
-        const dbStock = await dbCollection.findOne({ stock });
-        
-        const data = await response.json();
+        const dbStock = await stocksCollection.findOne({ stock });
 
         const responsePayload = {
           stockData: {
