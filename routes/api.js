a8aad0cca4fb327b64f11acfd11e88fef6d7d4f2
mateus-sartorio <mateussartorio39@gmail.com>
2024-12-23 21:47:26 +0000

    Feat: functional tests

--- routes/api.js
+++ routes/api.js
@@ -4,66 +4,128 @@ const crypto = require("crypto");
 
 require('dotenv').config();
 
+async function getUser(usersCollection, ipHash) {
+  let user = await usersCollection.findOne({ ipHash });
+
+  if(!user) {
+    await usersCollection.insertOne({
+      ipHash,
+      likes: []
+    });
+
+    user = await usersCollection.findOne({ ipHash });
+  }
+
+  return user;
+}
+
+async function likeStock(stocksCollection, usersCollection, stock, ipHash) {
+  const dbStock = await stocksCollection.findOne({ stock });
+
+  if(!dbStock) {
+    await stocksCollection.insertOne({
+      stock,
+      likes: 1
+    });
+  }
+  else {
+    await stocksCollection.updateOne({ stock }, {
+      $inc: { likes: 1 }
+    })
+  }
+
+  await usersCollection.updateOne({ ipHash }, {
+    $push: { likes: stock }
+  });
+}
+
+async function getSinglePriceResponse(stocksCollection, usersCollection, stock, like, ipHash) {
+  const response = await fetch(`${process.env.FREECODECAMP_API_BASE_URL}/v1/stock/${stock}/quote`);
+  const stockData = await response.json();
+
+  if(stockData === "Invalid symbol") {
+    throw new Error("invalid symbol");
+  }
+
+  const user = await getUser(usersCollection, ipHash);
+  
+  if(like && !user.likes.includes(stock)) {
+    await likeStock(stocksCollection, usersCollection, stock, ipHash);
+  }
+
+  const dbStock = await stocksCollection.findOne({ stock });
+
+  return {
+    stockData: {
+      stock,
+      price: stockData.latestPrice,
+      likes: dbStock?.likes ?? 0
+    }
+  };
+}
+
+async function getComparePricesResponse(stocksCollection, usersCollection, stocks, like, ipHash) {
+  const responses = await Promise.all(stocks.map(s => fetch(`${process.env.FREECODECAMP_API_BASE_URL}/v1/stock/${s}/quote`)));
+  const stocksData = await Promise.all(responses.map(r => r.json()));
+  
+  if(stocksData.some(sd => sd === "Invalid symbol")) {
+    throw new Error("invalid symbol");
+  }
+
+  const user = await getUser(usersCollection, ipHash);
+  
+  if(like) {
+    for(const s of stocks) {
+      if(!user.likes.includes(s)) {
+        await likeStock(stocksCollection, usersCollection, s, ipHash);
+      }
+    }
+  }
+
+  const dbStock1 = await stocksCollection.findOne({ stock: stocks[0] });
+  const dbStock2 = await stocksCollection.findOne({ stock: stocks[1] });
+
+  return {
+    stockData: [
+      {
+        stock: stocks[0],
+        price: stocksData[0].latestPrice,
+        rel_likes: (dbStock1?.likes ?? 0) - (dbStock2?.likes ?? 0)
+      },
+      {
+        stock: stocks[1],
+        price: stocksData[1].latestPrice,
+        rel_likes: (dbStock2?.likes ?? 0) - (dbStock1?.likes ?? 0)
+      }
+    ]
+  }
+}
+
 module.exports = function (app, dbClient) {
   app.route('/api/stock-prices')
     .get(async function (req, res) {
-      const stocksCollection = dbClient.db("stock-price-checker").collection("stocks");
-      const usersCollection = dbClient.db("stock-price-checker").collection("users");
-
       try {
         const { stock, like } = req.query;
 
         const ip = req.header("x-forwarded-for");
-
         const ipHash = crypto.createHash('md5').update(ip).digest('hex');
 
-        const response = await fetch(`${process.env.FREECODECAMP_API_BASE_URL}/v1/stock/${stock}/quote`);
-        const data = await response.json();
+        const dbName = ip === 'test' ? "stock-price-checker-test" : "stock-price-checker";
+          
+        const database = dbClient.db(dbName);
+      
+        const stocksCollection = database.collection("stocks");
+        const usersCollection = database.collection("users");
 
-        if(data === "Invalid symbol") {
-          throw new Error("invalid symbol");
+        let responsePayload;
+        if(Array.isArray(stock)) {
+          const stocks = stock.map(s => s.trim().toUpperCase());
+          responsePayload = await getComparePricesResponse(stocksCollection, usersCollection, stocks, like === 'true', ipHash);
         }
-
-        let dbUser = await usersCollection.findOne({ ipHash });
-
-        if(!dbUser) {
-          await usersCollection.insertOne({
-            ipHash,
-            likes: []
-          });
-
-          dbUser = await usersCollection.findOne({ ipHash });
+        else {
+          const parsedStock = stock.trim().toUpperCase();
+          responsePayload = await getSinglePriceResponse(stocksCollection, usersCollection, parsedStock, like === 'true', ipHash);
         }
-        
-        if(like === 'true' && !dbUser.likes.includes(stock)) {
-          const dbStock = await stocksCollection.findOne({ stock });
-
-          if(!dbStock) {
-            await stocksCollection.insertOne({
-              stock,
-              likes: 1
-            });
-          }
-          else {
-            await stocksCollection.updateOne({ stock }, {
-              $inc: { likes: 1 }
-            })
-          }
-
-          await usersCollection.updateOne({ ipHash }, {
-            $push: { likes: stock }
-          });
-        }
-
-        const dbStock = await stocksCollection.findOne({ stock });
-
-        const responsePayload = {
-          stockData: {
-            stock,
-            price: data.latestPrice,
-            likes: dbStock?.likes ?? 0
-          }
-        };
 
         res.json(responsePayload);
       }
@@ -78,4 +140,21 @@ module.exports = function (app, dbClient) {
         res.json(errorPayload);
       }
     });
+
+  app.route('/api/clear').post(async function (req, res) {
+    const ip = req.header("x-forwarded-for");
+
+    if(ip === 'test') {
+      const dbName = "stock-price-checker-test";
+      const database = dbClient.db(dbName);
+    
+      const stocksCollection = database.collection("stocks");
+      const usersCollection = database.collection("users");
+      
+      stocksCollection.deleteMany();
+      usersCollection.deleteMany();
+
+      res.end();
+    }
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,248 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  before(function (done) {
+    this.timeout(0);
 
+    chai
+    .request(server)
+    .keepOpen()
+    .post('/api/clear/')
+    .set('x-forwarded-for', 'test')
+    .end(() => {
+      done();
+    })
+  });
+
+  after(function (done) {
+    this.timeout(0);
+
+    chai
+    .request(server)
+    .keepOpen()
+    .post('/api/clear/')
+    .set('x-forwarded-for', 'test')
+    .end(() => {
+      done();
+    })
+  });
+
+  test("Viewing one stock: GET request to /api/stock-prices/", function (done) {
+    const input = {
+      stock: "GOOG",
+      like: false,
+    };
+    
+    chai
+      .request(server)
+      .keepOpen()
+      .get(`/api/stock-prices/?stock=${input.stock}&like=${input.like}`)
+      .set('x-forwarded-for', 'test')
+      .end((_err, res) => {
+        assert.equal(res.status, 200);
+
+        const output = res.body;
+
+        assert.property(output.stockData, 'stock');
+        assert.strictEqual(output.stockData.stock, 'GOOG');
+
+        assert.property(output.stockData, 'price');
+        assert.isTrue(Number.isFinite(output.stockData.price));
+        assert.isTrue(output.stockData.price >= 0);
+
+        assert.property(output.stockData, 'likes');
+        assert.isTrue(output.stockData.likes === 0);
+
+        done();
+      });
+  });
+
+  test("Viewing one stock and liking it: GET request to /api/stock-prices/", function (done) {
+    const input = {
+      stock: "GOOG",
+      like: true,
+    };
+    
+    chai
+      .request(server)
+      .keepOpen()
+      .get(`/api/stock-prices/?stock=${input.stock}&like=${input.like}`)
+      .set('x-forwarded-for', 'test')
+      .end((_err, res) => {
+        assert.equal(res.status, 200);
+
+        const output = res.body;
+
+        assert.property(output.stockData, 'stock');
+        assert.strictEqual(output.stockData.stock, 'GOOG');
+
+        assert.property(output.stockData, 'price');
+        assert.isTrue(Number.isFinite(output.stockData.price));
+        assert.isTrue(output.stockData.price >= 0);
+
+        assert.property(output.stockData, 'likes');
+        assert.isTrue(output.stockData.likes === 1);
+
+        done();
+      });
+  });
+
+  test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", function (done) {
+    const input = {
+      stock: "MSFT",
+      like: true,
+    };
+    
+    chai
+      .request(server)
+      .keepOpen()
+      .get(`/api/stock-prices/?stock=${input.stock}&like=${input.like}`)
+      .set('x-forwarded-for', 'test')
+      .end((_err, res) => {
+        assert.equal(res.status, 200);
+
+        const output = res.body;
+
+        assert.property(output.stockData, 'stock');
+        assert.strictEqual(output.stockData.stock, input.stock);
+
+        assert.property(output.stockData, 'price');
+        assert.isTrue(Number.isFinite(output.stockData.price));
+        assert.isTrue(output.stockData.price >= 0);
+
+        assert.property(output.stockData, 'likes');
+        assert.isTrue(output.stockData.likes === 1);
+        
+        chai
+          .request(server)
+          .keepOpen()
+          .get(`/api/stock-prices/?stock=${input.stock}&like=${input.like}`)
+          .set('x-forwarded-for', 'test')
+          .end((_err, res) => {
+            assert.equal(res.status, 200);
+
+            const output = res.body;
+
+            assert.property(output.stockData, 'stock');
+            assert.strictEqual(output.stockData.stock, input.stock);
+
+            assert.property(output.stockData, 'price');
+            assert.isTrue(Number.isFinite(output.stockData.price));
+            assert.isTrue(output.stockData.price >= 0);
+
+            assert.property(output.stockData, 'likes');
+            assert.isTrue(output.stockData.likes === 1);
+
+            done();
+          });
+      });
+  });
+
+  test("Viewing one stock: GET request to /api/stock-prices/", function (done) {
+    const input = {
+      stock1: "GOOG",
+      stock2: "AAPL",
+      like: false,
+    };
+    
+    chai
+      .request(server)
+      .keepOpen()
+      .get(`/api/stock-prices/?stock=${input.stock1}&stock=${input.stock2}&like=${input.like}`)
+      .set('x-forwarded-for', 'test')
+      .end((_err, res) => {
+        assert.equal(res.status, 200);
+
+        const output = res.body;
+
+        assert.property(output, 'stockData');
+        assert.isArray(output.stockData);
+
+        // Stock 1
+        const stock1 = output.stockData[0];
+
+        assert.isObject(stock1);
+
+        assert.property(stock1, 'stock');
+        assert.strictEqual(stock1.stock, input.stock1);
+
+        assert.property(stock1, 'price');
+        assert.isTrue(Number.isFinite(stock1.price));
+        assert.isTrue(stock1.price >= 0);
+
+        assert.property(stock1, 'rel_likes');
+        assert.isTrue(stock1.rel_likes === 1);
+
+        // Stock 2
+        const stock2 = output.stockData[1];
+
+        assert.isObject(stock2);
+
+        assert.property(stock2, 'stock');
+        assert.strictEqual(stock2.stock, input.stock2);
+
+        assert.property(stock2, 'price');
+        assert.isTrue(Number.isFinite(stock2.price));
+        assert.isTrue(stock2.price >= 0);
+
+        assert.property(stock2, 'rel_likes');
+        assert.isTrue(stock2.rel_likes === -1);
+
+        done();
+      });
+  });
+
+  test("Viewing two stocks and liking them: GET request to /api/stock-prices/", function (done) {
+    const input = {
+      stock1: "GOOG",
+      stock2: "AAPL",
+      like: true,
+    };
+    
+    chai
+      .request(server)
+      .keepOpen()
+      .get(`/api/stock-prices/?stock=${input.stock1}&stock=${input.stock2}&like=${input.like}`)
+      .set('x-forwarded-for', 'test')
+      .end((_err, res) => {
+        assert.equal(res.status, 200);
+
+        const output = res.body;
+
+        assert.property(output, 'stockData');
+        assert.isArray(output.stockData);
+
+        // Stock 1
+        const stock1 = output.stockData[0];
+
+        assert.isObject(stock1);
+
+        assert.property(stock1, 'stock');
+        assert.strictEqual(stock1.stock, input.stock1);
+
+        assert.property(stock1, 'price');
+        assert.isTrue(Number.isFinite(stock1.price));
+        assert.isTrue(stock1.price >= 0);
+
+        assert.property(stock1, 'rel_likes');
+        assert.isTrue(stock1.rel_likes === 0);
+
+        // Stock 2
+        const stock2 = output.stockData[1];
+
+        assert.isObject(stock2);
+
+        assert.property(stock2, 'stock');
+        assert.strictEqual(stock2.stock, input.stock2);
+
+        assert.property(stock2, 'price');
+        assert.isTrue(Number.isFinite(stock2.price));
+        assert.isTrue(stock2.price >= 0);
+
+        assert.property(stock2, 'rel_likes');
+        assert.isTrue(stock2.rel_likes === 0);
+
+        done();
+      });
+  });
 });
